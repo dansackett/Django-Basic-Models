c41e0a892e3853ded566939cbc139bb7f01521fd
Keith Philpott <keith@digitalbutter.com>
2014-11-18 13:02:17 -0800

    re-formatted suffix

--- person.py
+++ person.py
@@ -31,14 +31,20 @@ class Person(models.Model):
     def __str__(self):
         return self.full_name_forward
 
-    def _get_full_name(self, last_name_first=False, include_middle_name=False):
+    def _get_full_name(self, last_name_first=False, include_middle_name=False, include_suffix=True):
         if last_name_first:
-            return ', '.join([name for name in (self.get_last_name_with_suffix(),
+            name = ', '.join([name for name in (self.last_name,
                                                 self.get_first_name_with_prefix(include_middle_name=include_middle_name)
                                                 ) if name])
         else:
-            return ' '.join([name for name in (self.get_first_name_with_prefix(include_middle_name=include_middle_name),
-                                               self.get_last_name_with_suffix()) if name])
+            name = ' '.join([name for name in (self.get_first_name_with_prefix(include_middle_name=include_middle_name),
+                                               self.last_name) if name])
+
+        if include_suffix and self.suffix:
+            if self.suffix_requires_comma:
+                name = "{},".format(name)
+            name = "{} {}".format(name, self.suffix)
+        return name
 
     def get_first_name_with_prefix(self, include_middle_name=False):
         """
@@ -50,14 +56,6 @@ class Person(models.Model):
         if include_middle_name:
             name_list.append(self.middle_name)
         return ' '.join([name for name in name_list if name])
-
-    def get_last_name_with_suffix(self):
-        """
-
-        :return: <lastname>, <suffix>
-        """
-        join_str = ', ' if self.suffix_requires_comma else ' '
-        return join_str.join([name for name in (self.last_name, self.suffix) if name])
 
     @property
     def full_name_forward(self):
@@ -91,7 +89,6 @@ class Person(models.Model):
     @property
     def age(self):
         """
-
         :return: an integer
         """
         if not self.birth_date:
